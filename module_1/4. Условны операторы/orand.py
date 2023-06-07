95ba3344bb955d0715f1bd0111a1427b23484b75
Norica <a.kolpakov2010@gmail.com>
2023-06-08 04:29:28 +0700

    Pres4 finished

---
+++ module_1/4. Условны операторы/orand.py
@@ -0,0 +1,10 @@
+OR = True or False or False or False #...
+print(OR)
+
+AND = False and True and True and True #...
+print(AND)
+
+
+res = True and (False or True) and False
+# ==
+res = 1 * (0 + 1) * 0
