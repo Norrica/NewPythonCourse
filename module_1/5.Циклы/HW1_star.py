fc79681ba46e5834bc76f37fc25a7308d9172263
Norica <a.kolpakov2010@gmail.com>
2023-06-11 10:18:35 +0700

    Separate homework 1 into basic and advanced tasks

--- module_1/5.Циклы/HW1_star.py
+++ module_1/5.Циклы/HW1_star.py
@@ -1,13 +1,18 @@
 import random
 
 num = random.randint(1, 100)
-for i in range(5):  # !!!
+tries = 5
+for i in range(tries):
+    print(f'У тебя осталось {tries} попыток')
+    tries -= 1
     answer = int(input('Какое число я загадал? '))
     if answer > num:
         print('Моё число меньше!')
     elif answer < num:
         print('Моё число больше!')
     else:
-        print('Угадал!')
         break
-
+if tries == 0:
+    print('Ты проиграл')
+else:
+    print('Ты выиграл!')
