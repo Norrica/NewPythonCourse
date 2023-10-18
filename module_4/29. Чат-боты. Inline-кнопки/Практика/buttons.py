fd5e30a4a3ebf67567e8965b76d19c78029d7130
Aleksey Redkous <ronmount@ya.ru>
2023-10-19 02:31:29 +0300

    Урок 29

---
+++ module_4/29. Чат-боты. Inline-кнопки/Практика/buttons.py
@@ -0,0 +1,22 @@
+from pyrogram.types import KeyboardButton, InlineKeyboardButton
+from pyrogram import emoji
+
+# Общие кнопки
+back_button = KeyboardButton(f"{emoji.BACK_ARROW} Назад")
+
+# Кнопки главного меню
+time_button = KeyboardButton(f"{emoji.ALARM_CLOCK} Время")
+help_button = KeyboardButton(f"{emoji.WHITE_QUESTION_MARK} Помощь")
+settings_button = KeyboardButton(f"{emoji.GEAR} Настройки")
+weather_button = KeyboardButton(f"{emoji.SUN_BEHIND_CLOUD} Погода")
+cats_button = KeyboardButton(f"{emoji.CAT} Котики")
+
+# Кнопки настроек
+change_city_button = KeyboardButton(f"{emoji.CITYSCAPE} Изменить город")
+
+# Инлайн кнопки
+weather_current_inline_button = InlineKeyboardButton(f"{emoji.FIVE_O_CLOCK} Погода сейчас", callback_data="weather_current")
+weather_forecast_inline_button = InlineKeyboardButton(f"{emoji.CALENDAR} Прогноз погоды", callback_data="weather_forecast")
+
+# Кнопки для котиков
+cats_random_inline_button = InlineKeyboardButton(f"{emoji.CAT} Случайный котик", callback_data="cats_random")
